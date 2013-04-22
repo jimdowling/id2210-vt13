513f10b0076c6a93d726f8906efcdc5091f66602
jdownling <jdownling@DowlingKTH.mshome.net>
2013-04-22 06:44:59 +0200

    P2P orchestrator added

--- search/src/main/java/search/main/Main.java
+++ search/src/main/java/search/main/Main.java
@@ -1,6 +1,6 @@
 package search.main;
 
-import search.simulator.core.SearchSimulationMain;
+import search.simulator.core.SearchExecutionMain;
 import common.configuration.Configuration;
 import common.simulation.scenarios.Scenario;
 import common.simulation.scenarios.Scenario1;
@@ -9,9 +9,9 @@ public class Main {
 	public static void main(String[] args) throws Throwable {
 		Configuration configuration = new Configuration();
 		configuration.set();
-		
+
 		Scenario scenario = new Scenario1();
 		scenario.setSeed(System.currentTimeMillis());
-		scenario.getScenario().execute(SearchSimulationMain.class);
+		scenario.getScenario().execute(SearchExecutionMain.class);
 	}
 }
